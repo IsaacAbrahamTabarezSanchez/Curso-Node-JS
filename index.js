8d9f89007a7b6f3efab01e60659c96bad0aed0e4
Isaac Abraham Tabarez Sanchez <62556383+IsaacAbrahamTabarezSanchez@users.noreply.github.com>
2023-04-26 20:41:29 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const http= require ("http");
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function(req, res){
+    res.setHeader("Contet-type", "text/html");
+    res.end('<html><body><h1>Lalito <3 <h1><body><html>')
+    
+};
+
+const server  = http.createServer(requestListener);
+
+server.listen(port,host,()=>{
+    console.log(`Server is running on http://${host}:${port}`);
+})
